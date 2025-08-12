5e3366b72641a503c619b4d5f8613be444e7f460
XxSamridhaxX <SamridhaRB@gmail.com>
2025-08-12 20:58:37 +0545

    ...

--- store/models.py
+++ store/models.py
@@ -8,3 +8,24 @@ class Customer(models.Model):
     email = models.EmailField(max_length=200)
     def __str__(self):
         return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=60)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank = True)
+
+    def __str__(self):
+        return self.name
+# This order is like a cart where the things you ordered are placed
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null = True, blank = True)
+    date_ordered = models.DateTimeField(auto_now_add= True)
+    complete = models.BooleanField(default = False)
+    transaction = models.BooleanField(max_length = 100, null=True)
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete = models.SET_NULL, null = True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0,null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
