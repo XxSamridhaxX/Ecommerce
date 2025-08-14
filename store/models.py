6845299b7ffae69cd99164291f399f644ed7c374
XxSamridhaxX <SamridhaRB@gmail.com>
2025-08-14 15:37:49 +0545

    Cart Completed

--- store/models.py
+++ store/models.py
@@ -21,6 +21,15 @@ class Product(models.Model):
     def __str__(self):
         return self.name
     
+
+    @property
+    def ImageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
+    
     # def save(self,*args, **kwargs):
     #     super().save(*args, **kwargs)
 
@@ -39,6 +48,7 @@ class Product(models.Model):
     Thumbnail resizes the picture without stretching or distorting
     img.save means saving into same path where the image is resized
  """
+
     
 
 # This order is like a cart where the things you ordered are placed
@@ -46,13 +56,35 @@ class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null = True, blank = True)
     date_ordered = models.DateTimeField(auto_now_add= True)
     complete = models.BooleanField(default = False)
-    transaction = models.BooleanField(max_length = 100, null=True)
+    transaction_id = models.CharField(max_length = 100, null=True)
+
+    def __str__(self):
+        return str(self.id)
+    
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total\
+        
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total_count = sum([item.quantity for item in orderitems])
+        return total_count
+
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete = models.SET_NULL, null = True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField(default=0,null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 
 class ShippingAddress(models.Model):
--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Order, OrderItem
+from django.db.models import F,ExpressionWrapper, FloatField
 # Create your views here.
 
 def store(request):
@@ -9,11 +10,19 @@ def store(request):
 
 
 def cart(request):
-    context= {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer = customer, complete = False)
+        items = order.orderitem_set.all()
+        # items = items.annotate(total = ExpressionWrapper(F('quantity')*F('product__price'),output_field=FloatField()))
+        # Expression wrapper uses double underscore to traverse relationships
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+    context= {'items':items,'order':order}
     return render(request,'store/cart.html',context)
 
 
 def checkout(request):
     context= {}
     return render(request,'store/checkout.html',context)
-
