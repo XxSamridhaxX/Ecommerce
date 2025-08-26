dbf5bf61707db47a202dabb666f93517d4fdd6e7
XxSamridhaxX <SamridhaRB@gmail.com>
2025-08-26 08:52:37 +0545

    Added cookie functioality to the anonymous users

--- static/js/cart.js
+++ static/js/cart.js
@@ -8,7 +8,7 @@ for (var i = 0; i < updateBtns.length; i++) {
 
         console.log('USER:', user)
 if (user == 'AnonymousUser'){
-	console.log('User is not authenticated')
+	addCookieItems(productId, action)
 			
 }else{
 	updateUserOrder(productId, action)
@@ -40,3 +40,40 @@ function updateUserOrder(productId, action){
 		});
 }
 
+function addCookieItems(productId, action)
+{
+	console.log('User is not authenticated')
+
+	if (action == 'add')
+	{
+		// if cart object ma productId name ko attribute ma kei value chaina bhane/ exist gardeina bhane
+		if (cart[productId]== undefined)
+		{
+			//productId attribute ko euta nikalne ani quantity ma halne
+			cart[productId] = {'quantity':1}
+		}
+		else
+		{
+			//if paile dekhi nai cha bhane add action auda hataune
+			cart[productId]['quantity']+=1
+		}
+	}
+
+	if(action == 'remove')
+	{
+		cart[productId]-=1
+
+		if(cart[productId]['quantity']<=0)
+		{
+			console.log('Items Should be deleted')
+			delete cart[productId]
+		}
+
+			
+	}
+
+	console.log('art:',cart)
+	document.cookie = 'cart='+JSON.stringify(cart) + ";domain=;path=/"
+	location.reload()
+
+}
